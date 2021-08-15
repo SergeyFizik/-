9b73df28ea693d00722272c2bd5d0873e6b194bd
SergeyFizik <vax_serg@mail.ru>
2021-08-15 12:02:25 +0300

    lesson02

---
+++ scripts/scroipt.js
@@ -0,0 +1,16 @@
+let money=30000;
+let income='freelance';
+let addExpence='интернет, Такси, Коммуналка';
+let deposit=true;
+let mission= 1000000;
+let period=8;
+let budgetDay=money/30;
+
+console.log("money=",typeof money, "income=",typeof  income, "deposit=",typeof  deposit);
+console.log(addExpence.length);
+console.log("Период равен ", period, " месяцев");
+console.log("Цель заработать ", mission, " тугриков");
+console.log(addExpence.toLowerCase());
+console.log(addExpence.split());
+console.log(budgetDay);
+
