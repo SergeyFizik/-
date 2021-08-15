f73231934cce87f92e3c48033c5efc3d5457740a
SergeyFizik <vax_serg@mail.ru>
2021-08-15 11:46:04 +0300

    Lesson1

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,2 +1,8 @@
-alert('Изменения');
-console.log('работа');
+let money=300;
+let income=100;
+let addExpence=50;
+let deposit=10;
+let mission=1000;
+let period=12;
+alert('Не любой текcт');
+console.log('Совсем не любой текст');
