b12fafc700e93cd8bbd5590d61fed14a2e7c1507
SergeyFizik <vax_serg@mail.ru>
2021-08-17 12:35:40 +0300

    lesson03

--- scripts/scroipt.js
+++ scripts/scroipt.js
@@ -1,16 +1,30 @@
-let money=30000;
+let money=+prompt('Ваш месячный доход?');
 let income='freelance';
-let addExpence='интернет, Такси, Коммуналка';
-let deposit=true;
+let addExpence=prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет');
+let deposit=confirm('Есть ли у вас депозит в банке?');
+
+let expense1=prompt('Введите обязательную статью расходов:');
+let amount1=+prompt('Во сколько это обойдется?');
+let expense2=prompt('Введите обязательную статью расходов:');
+let amount2=+prompt('Во сколько это обойдется?');
 let mission= 1000000;
-let period=8;
-let budgetDay=money/30;
+let budgetMonth=money-amount1-amount2;
+console.log('Бюджет на месяц:', budgetMonth);
+let period=Math.ceil(mission/budgetMonth);
+let budgetDay=Math.floor(budgetMonth/30);
 
 console.log("money=",typeof money, "income=",typeof  income, "deposit=",typeof  deposit);
 console.log(addExpence.length);
-console.log("Период равен ", period, " месяцев");
+console.log("Период равен ", period, " месяцев(-а)");
 console.log("Цель заработать ", mission, " тугриков");
 console.log(addExpence.toLowerCase());
 console.log(addExpence.split());
-console.log(budgetDay);
+console.log('Бюджет на месяц: ',budgetDay);
 
+if (budgetDay>=1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay>=600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay>=0) {
+     console.log('К сожалению, у вас уровень дохода хреновый');
+} else { console.log('К вам скоро придут коллекторы');}
